d7f2217a393b142fb7f7c17f44db98c419ec251b
Paul Bellamy <paul.a.bellamy@gmail.com>
2011-02-15 18:07:02 +0000

    Adding root module and spec for it

---
+++ spec/pachube-data-formats_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe PachubeDataFormats do
+  it "should be defined" do
+    PachubeDataFormats.should_not be_nil
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 require 'rubygems'
 require 'bundler/setup'
+
+Dir['./spec/support/**/*.rb'].map {|f| require f}
+
+$:.push File.expand_path("../lib", __FILE__)
+require 'pachube-data-formats'
