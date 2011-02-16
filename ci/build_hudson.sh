03080a7216c6c17f5864110b83e274849e706896
Paul Bellamy <paul.a.bellamy@gmail.com>
2011-02-16 10:03:50 +0000

    Working ont he hudson build script

--- ci/build_hudson.sh
+++ ci/build_hudson.sh
@@ -6,15 +6,18 @@ set -x
 #TODO: script the installation of the pre-reqs: sqlite3
 #sudo apt-get install sqlite3 libsqlite3-dev libsqlite3-ruby
 
-outdir=`pwd`
-echo "started build script $0 in `pwd` at `date`"
+outdir='build_artefacts'
+rm -rf $outdir
+mkdir -p $outdir
+
+echo "started build script $0 in `$outdir` at `date`"
 
 set +e
 ~/sysadmin/show_platform/show_platform.sh > $outdir/platform.txt 2>&1
 set -e
 
 echo "Running custom build.sh - install bundle, rake"
-bundle install --deployment --path=.gem/
+bundle --path=.gem/
 set +e
 rake
 #> $outdir/testtask.log
