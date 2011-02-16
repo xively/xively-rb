4475ca17882824d620a966816ca82579ae94c4dc
Paul Bellamy <paul.a.bellamy@gmail.com>
2011-02-16 15:38:57 +0000

    Splitting up output and input tests, as they should be independent

--- spec/pachube_data_formats/feed_spec.rb
+++ spec/pachube_data_formats/feed_spec.rb
@@ -4,28 +4,30 @@ describe PachubeDataFormats::Feed do
   INPUT_FORMATS = %w(json)
   OUTPUT_FORMATS = %w(json)
 
-  INPUT_FORMATS.each do |input_format|
-    context "from #{input_format}" do
+  INPUT_FORMATS.each do |format|
+    context "input from #{format}" do
       describe "#initialize" do
         it "should accept one parameter" do
-          lambda{PachubeDataFormats::Feed.new(feed_as_(input_format))}.should_not raise_exception
+          lambda{PachubeDataFormats::Feed.new(feed_as_(format))}.should_not raise_exception
         end
 
         it "should ignore unknown fields"
-        #   feed = PachubeDataFormats::Feed.new({:title => "my name is", :bunny => "is a terrorist"})
-        #   feed.hash[:bunny].should be_nil
-        # end
+        #  feed = PachubeDataFormats::Feed.new({:title => "my name is", :bunny => "is a terrorist"})
+        #  feed.hash[:bunny].should be_nil
+        #end
       end
+    end
+  end
 
-      OUTPUT_FORMATS.each do |output_format|
-        describe "#to_#{output_format}" do
-          it "should output Pachube #{output_format}" do
-            feed = PachubeDataFormats::Feed.new(feed_as_(input_format))
-            json = feed.send("to_#{output_format}")
-            json.should_not be_nil
-            parsed_json = JSON.parse(json)
-            parsed_json.should == JSON.parse(feed_as_(input_format))
-          end
+  OUTPUT_FORMATS.each do |format|
+    context "output to #{format}" do
+      describe "#to_#{format}" do
+        it "should output Pachube #{format}" do
+          feed = PachubeDataFormats::Feed.new(feed_as_("json"))
+          json = feed.send("to_#{format}")
+          json.should_not be_nil
+          parsed_json = JSON.parse(json)
+          parsed_json.should == JSON.parse(feed_as_("json"))
         end
       end
     end
