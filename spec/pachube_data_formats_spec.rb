cbc380a38aad79d94ec5feec83cae4d943d64fda
Levent Ali <lebreeze@gmail.com>
2011-02-17 17:10:46 +0000

    Added tests for Generator/Parser template

--- spec/pachube_data_formats_spec.rb
+++ spec/pachube_data_formats_spec.rb
@@ -1,7 +1,17 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 
 describe PachubeDataFormats do
-  it "should be defined" do
-    PachubeDataFormats.should_not be_nil
+  it "should provide a template for Generator classes" do
+    class TestGenerator
+      extend PachubeDataFormats::Generator
+    end
+    lambda{TestGenerator.generate("")}.should raise_error(RuntimeError, "Implement self.generate(hash)")
+  end
+
+  it "should provide a template for Parser classes" do
+    class TestParser
+      extend PachubeDataFormats::Parser
+    end
+    lambda{TestParser.parse("")}.should raise_error(RuntimeError, "Implement self.parse(input)")
   end
 end
