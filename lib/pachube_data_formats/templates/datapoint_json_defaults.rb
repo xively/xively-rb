e49a4a213974beb1885d055e0cda5d902ea0dd5a
Levent Ali <lebreeze@gmail.com>
2011-03-03 18:07:10 +0000

    Bug fix: Datapoint json template wasn't formatting time to iso8601(6)

--- lib/pachube_data_formats/templates/datapoint_json_defaults.rb
+++ lib/pachube_data_formats/templates/datapoint_json_defaults.rb
@@ -3,7 +3,7 @@ module PachubeDataFormats
     module DatapointJSONDefaults
       def generate_json
         template = Template.new(self, :json)
-        template.at
+        template.at {at.iso8601(6)}
         template.value
         template.output!
       end
--- spec/pachube_data_formats/templates/datapoint_json_defaults_spec.rb
+++ spec/pachube_data_formats/templates/datapoint_json_defaults_spec.rb
@@ -8,7 +8,7 @@ describe "default datapoint json templates" do
   it "should represent Pachube datapoints (only used by API v2)" do
     json = @datapoint.generate_json
     json[:value].should == @datapoint.value
-    json[:at].should == @datapoint.at
+    json[:at].should == @datapoint.at.iso8601(6)
   end
 end
 
