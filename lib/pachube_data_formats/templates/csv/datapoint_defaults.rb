f0e40ef5eebcb32dff4d79d08cc82e4308728db1
Levent Ali <lebreeze@gmail.com>
2011-03-09 13:44:58 +0000

    Adding depth option to datapoint#to_csv

--- lib/pachube_data_formats/templates/csv/datapoint_defaults.rb
+++ lib/pachube_data_formats/templates/csv/datapoint_defaults.rb
@@ -3,8 +3,14 @@ module PachubeDataFormats
     module CSV
       module DatapointDefaults
         def generate_csv(version, options = {})
-          if options[:full]
+          options[:depth] = 4 if options[:full]
+          case options[:depth].to_i
+          when 4
             ::CSV.generate_line([feed_id, datastream_id, at.iso8601(6), value])
+          when 3
+            ::CSV.generate_line([datastream_id, at.iso8601(6), value])
+          when 2
+            ::CSV.generate_line([at.iso8601(6), value])
           else
             ::CSV.generate_line([value])
           end
--- spec/pachube_data_formats/templates/csv/datapoint_defaults_spec.rb
+++ spec/pachube_data_formats/templates/csv/datapoint_defaults_spec.rb
@@ -10,6 +10,17 @@ describe "default datapoint json templates" do
     csv.should == @datapoint.value
   end
 
+  it "should accept a depth option" do
+    csv = @datapoint.generate_csv("2", :depth => 1)
+    csv.should == @datapoint.value
+    csv = @datapoint.generate_csv("2", :depth => 2)
+    csv.should == [@datapoint.at.iso8601(6),@datapoint.value].join(',')
+    csv = @datapoint.generate_csv("2", :depth => 3)
+    csv.should == [@datapoint.datastream_id, @datapoint.at.iso8601(6),@datapoint.value].join(',')
+    csv = @datapoint.generate_csv("2", :depth => 4)
+    csv.should == [@datapoint.feed_id, @datapoint.datastream_id, @datapoint.at.iso8601(6), @datapoint.value].join(',')
+  end
+
   it "should have an optional full representation" do
     csv = @datapoint.generate_csv("2", :full => true)
     csv.should == [@datapoint.feed_id, @datapoint.datastream_id, @datapoint.at.iso8601(6), @datapoint.value].join(',')
