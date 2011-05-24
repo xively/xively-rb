f4941a441c96817b1f8e1c08b2a615d48fc69c21
Levent Ali <lebreeze@gmail.com>
2011-05-24 12:21:51 +0100

    Bumping version to 0.1.01

--- lib/pachube_data_formats/version.rb
+++ lib/pachube_data_formats/version.rb
@@ -1,8 +1,8 @@
 module PachubeDataFormats #:nodoc:
   module VERSION
     MAJOR = 0
-    MINOR = 0
-    TINY  = 3
+    MINOR = 1
+    TINY  = '01'
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
