2169c381c22bc873efe8c6ab2f45c8e2d0c2b8c1
Sam Mulube <sam@pachube.com>
2011-09-20 23:17:09 +0000

    Altered keys stuff to deal with the new format JSON/XML.
    Test coverage almost back up to 100%, but there's a line I haven't
    figured out how to it yet, or rather looking at the test is appears as
    if it should be being reached, but according to rcov isn't.
    
    Hell of a lot of moving parts in pachube_data_formats, so I hope I've
    got all the bits.

--- lib/pachube_data_formats/templates/json/key_defaults.rb
+++ lib/pachube_data_formats/templates/json/key_defaults.rb
@@ -4,19 +4,37 @@ module PachubeDataFormats
       module KeyDefaults
         def generate_json(options={})
           template = Template.new(self, :json)
-          template.key do
+          template.key do |k|
+            if self.scopes
+              s = self.scopes.collect { |s|
+                if s.resources
+                  res = s.resources.collect { |r|
+                    {
+                      :feed_id => r.feed_id,
+                      :datastream_id => r.datastream_id,
+                      :datastream_trigger_id => r.datastream_trigger_id
+                    }.delete_if_nil_value
+                  }
+                end
+                {
+                  :referer => s.referer,
+                  :source_ip => s.source_ip,
+                  :private_access => s.private_access,
+                  :label => s.label,
+                  :minimum_interval => s.minimum_interval,
+                  :permissions => s.permissions.collect { |p| p.to_s.downcase },
+                  :resources => res
+                }.delete_if_nil_value
+              }
+            end
+
             {
               :id => id,
               :expires_at => expires_at,
-              :feed_id => feed_id,
               :api_key => key,
-              :permissions => permissions,
-              :private_access => private_access,
-              :referer => referer,
-              :source_ip => source_ip,
-              :datastream_id => datastream_id,
               :user => user,
-              :label => label
+              :label => label,
+              :scopes => s
             }
           end
           template.output! options
