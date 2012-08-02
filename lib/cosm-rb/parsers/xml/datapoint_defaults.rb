1e4d54134d1adfe028f60e1aaeee51333e3c5344
Sam Mulube <sam@pachube.com>
2012-08-02 14:03:06 +0000

    Catch XML parsing errors everywhere

--- lib/cosm-rb/parsers/xml/datapoint_defaults.rb
+++ lib/cosm-rb/parsers/xml/datapoint_defaults.rb
@@ -7,18 +7,18 @@ module Cosm
             xml = Nokogiri::XML(xml) do |config|
               config.strict.nonet
             end
+            hash = {}
+            environment = xml.at_xpath("//xmlns:environment")
+            raise InvalidXMLError, "Missing 'environment' node from base node" if environment.nil?
+            data = environment.at_xpath("xmlns:data")
+            datapoint = data.at_xpath("xmlns:datapoints")
+            value = datapoint.at_xpath("xmlns:value")
+            hash["value"] = value.content
+            hash["at"] = value.attributes["at"].value
+            hash
           rescue Nokogiri::SyntaxError => e
             raise InvalidXMLError, e.message
           end
-          hash = {}
-          environment = xml.at_xpath("//xmlns:environment")
-          raise InvalidXMLError, "Missing 'environment' node from base node" if environment.nil?
-          data = environment.at_xpath("xmlns:data")
-          datapoint = data.at_xpath("xmlns:datapoints")
-          value = datapoint.at_xpath("xmlns:value")
-          hash["value"] = value.content
-          hash["at"] = value.attributes["at"].value
-          hash
         end
       end
     end
